972f261f6fcf3c3acf83e0211a02d4e7cc8e039f
Mowafak Allaham <mmallaham1@gmail.com>
2022-07-16 18:48:12 -0500

    fix references

--- pyergm/ergm.py
+++ pyergm/ergm.py
@@ -1,6 +1,6 @@
 from rpy2.robjects import Formula
-from rpy_interface import intializeRenv
-from helper import timer_func
+from .rpy_interface import intializeRenv
+from .helper import timer_func
 import logging
 
 class pyERGM:
